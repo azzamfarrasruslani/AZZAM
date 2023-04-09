f255d6bc63854cae49ba615f72cd550774315495
Azzam Farras Ruslani <azzamfarrasrusl@gmail.com>
2023-04-09 18:15:24 +0700

    add folder

---
+++ belajar java/belajar.js
@@ -0,0 +1,12 @@
+//alert('hi saya azzam')
+
+//console.log('hi saya azzam')
+
+//prompt('masukan kata sandi')
+
+//var usia = 18
+//console.log('berapa usia kamu adalah'+ usia)
+
+let nama= 'azzam farras ruslani'
+let usia = 100
+alert('nama saya adalah'+ nama +'dan usia saya'+ usia)
